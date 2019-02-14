8a46cf2ddf4db8c37072855010f7399bdd39f81a
jkovba <jkovba@gmail.com>
2019-02-14 19:37:05 -0400

    Create insert_data.php

---
+++ insert_data.php
@@ -0,0 +1,33 @@
+<?php
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+$dbname = "jhu";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO students (first_name, last_name, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$sql = "INSERT INTO students (first_name, last_name, email)
+VALUES ('Joe', 'kovba', 'joe@example.com')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
